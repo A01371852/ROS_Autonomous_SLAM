a200d0c9bd31c7c72ab1b10277f942693c276c32
Emmanuel Hernández <A01371852@itesm.mx>
2019-11-22 13:34:04 -0600

    Modified navigation source code.

--- catkin_ws/src/X80_navigation/src/send_goals.cpp
+++ catkin_ws/src/X80_navigation/src/send_goals.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <string.h>
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
@@ -8,18 +9,18 @@ class Listener  // Odometry subscriber
 {
 	public:
 		double stamp;
-        double x;
-        double y;
-        double w;
+      double x;
+      double y;
+      double w;
 	void callback(const nav_msgs::Odometry::ConstPtr& odom);
 };
 
 void Listener::callback(const nav_msgs::Odometry::ConstPtr& odom)
 {
 	stamp = odom->header.stamp.toSec();
-    x = odom->pose.pose.position.x;
-    y = odom->pose.pose.position.y;
-    w = odom->pose.pose.orientation.w;
+  x = odom->pose.pose.position.x;
+  y = odom->pose.pose.position.y;
+  w = odom->pose.pose.orientation.w;
 }
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -30,8 +31,8 @@ int main(int argc, char** argv){
   ros::NodeHandle n;
 
   // 'drrobot_X80/odom' topic suscriber
-  Listener listener;
-  ros::Subscriber odom_sub = n.subscribe<nav_msgs::Odometry>("drrobot_X80/odom", 1, &Listener::callback,&listener);
+  Listener odom;
+  ros::Subscriber odom_sub = n.subscribe<nav_msgs::Odometry>("drrobot_X80/odom", 1, &Listener::callback,&odom);
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -47,39 +48,51 @@ int main(int argc, char** argv){
   goal.target_pose.header.frame_id = "base_link";
   goal.target_pose.header.stamp = ros::Time::now();
 
-  goal.target_pose.pose.position.x = 0.5;
-  goal.target_pose.pose.position.y = 0.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+  //double timeout = 40.0;  // secs
+  double timeout = 3600.0;  // secs
 
-  double x = goal.target_pose.pose.position.x;
-  double y = goal.target_pose.pose.position.y;
-  double w = goal.target_pose.pose.orientation.w;
+  double x = -10.0;         // Set goal
+  double y = 0.0;
+  double w = -1.0;
 
-  ROS_INFO("%f",x);
-  ROS_INFO("%f",y);
-  ROS_INFO("%f",w);
+  double last_time = ros::Time::now().toSec();
 
+  goal.target_pose.pose.position.x = x; // Send goal
+  goal.target_pose.pose.position.y = y;
+  goal.target_pose.pose.orientation.w = w;
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
+  ros::Duration(0.5).sleep();   // Sleep for half a second
 
   // Loop rate
   ros::Rate loop_rate(1);	// 1Hz
 
-  //ac.waitForResult( ros::Duration(30, 30));
   while (ros::ok()) {
+
+    //ROS_INFO("time: %f,\tdist: %f,\trot: %f", odom.stamp-last_time, pow( pow(odom.x - x, 2) + pow(odom.y - y, 2), 0.5), fabs(odom.w - w));
+    ROS_INFO("result: %d", system(ac.getState().getText().c_str()));
     
-    //ROS_INFO("X: %f,\tY: %f", listener.x, listener.y);
-    ROS_INFO("dist: %f,\trot: %f", pow( pow(listener.x - x, 2) + pow(listener.y - y, 2), 0.5), fabs(listener.w - w));
-    
-    if( pow( pow(listener.x - x, 2) + pow(listener.y - y, 2), 0.5) < 0.1 && fabs(listener.w - w) < 0.1){
-        ROS_INFO("Hooray, the base moved 1 meter forward");
-        ac.cancelAllGoals();
-        break;
+    if( pow( pow(odom.x - x, 2) + pow(odom.y - y, 2), 0.5) < 0.1  // Distance from goal
+      && fabs(odom.w - w) < 0.1 )                                 // Difference in orientation
+    {
+      ac.cancelAllGoals();
+      ROS_INFO("\n\tGoal reached");
+      ros::Duration(1.0).sleep();   // Sleep for a second
+      break;
     }
-    
+    else if ( odom.stamp - last_time > timeout || ac.getState() == actionlib::SimpleClientGoalState::ABORTED)  // Timeout reached
+    {
+      ac.cancelAllGoals();
+      ROS_INFO("\n\tCould not reach goal");
+      ros::Duration(1.0).sleep();   // Sleep for a second
+      break;
+    }
+
     ros::spinOnce();
     loop_rate.sleep();
   }
+
+  last_time = odom.stamp;
 
   return 0;
 }
