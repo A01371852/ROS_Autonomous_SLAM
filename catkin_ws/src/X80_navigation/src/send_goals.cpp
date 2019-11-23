41e3f20220eabd765df99a44a2e1476fd4ecd265
Emmanuel Hernández <A01371852@itesm.mx>
2019-11-23 16:10:57 -0600

    Modified navigation node.

--- catkin_ws/src/X80_navigation/src/send_goals.cpp
+++ catkin_ws/src/X80_navigation/src/send_goals.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <math.h>
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
@@ -14,9 +15,9 @@ class OdomListener  // Odometry subscriber
     double x;
     double y;
     double w;
-	void callback(const nav_msgs::Odometry::ConstPtr& odom);
+	void odom_callback(const nav_msgs::Odometry::ConstPtr& odom);
 };
-void OdomListener::callback(const nav_msgs::Odometry::ConstPtr& odom)
+void OdomListener::odom_callback(const nav_msgs::Odometry::ConstPtr& odom)
 {
 	stamp = odom->header.stamp.toSec();
   x = odom->pose.pose.position.x;
@@ -31,25 +32,20 @@ class GridListener  // Gridmap subscriber
     double resolution;
     int width;
     int height;
-    int** grid;
     geometry_msgs::Point pos;
     geometry_msgs::Quaternion dir;
-    //std::vector<signed char, std::allocator<signed char> > data;
-	void callback(const nav_msgs::OccupancyGrid::ConstPtr& gridmap);
+    std::vector<signed char, std::allocator<signed char> > data;
+	void grid_callback(const nav_msgs::OccupancyGrid::ConstPtr& gridmap);
 };
-void GridListener::callback(const nav_msgs::OccupancyGrid::ConstPtr& gridmap)
-{  
+void GridListener::grid_callback(const nav_msgs::OccupancyGrid::ConstPtr& gridmap)
+{
   stamp = gridmap->header.stamp.toSec();
   resolution = gridmap->info.resolution;
-  width = gridmap->info.width;
+  width = gridmap->info.width;;
   height = gridmap->info.height;
-  grid[width][height];
-  for(int i = 0; i < height; i++)
-    for(int j = 0; j < height; j++)
-      grid[i][j] = (int)gridmap->data[i+j];
   pos = gridmap->info.origin.position;
   dir = gridmap->info.origin.orientation;
-  //data = gridmap->data;
+  data = gridmap->data;
 }
 
 // move_base publisher
@@ -63,11 +59,11 @@ int main(int argc, char** argv){
 
   // 'drrobot_X80/odom' topic suscriber
   OdomListener odom;
-  ros::Subscriber odom_sub = n_odom.subscribe<nav_msgs::Odometry>("drrobot_X80/odom", 1, &OdomListener::callback,&odom);
+  ros::Subscriber odom_sub = n_odom.subscribe<nav_msgs::Odometry>("drrobot_X80/odom", 1, &OdomListener::odom_callback,&odom);
 
   // 'rtabmap/grid_map' topic suscriber
   GridListener gridmap;
-  ros::Subscriber gridmap_sub = n_gridmap.subscribe<nav_msgs::OccupancyGrid>("rtabmap/grid_map", 1, &GridListener::callback,&gridmap);
+  ros::Subscriber gridmap_sub = n_gridmap.subscribe<nav_msgs::OccupancyGrid>("rtabmap/grid_map", 1, &GridListener::grid_callback,&gridmap);
   /*nav_msgs/OccupancyGrid*/
 
   //tell the action client that we want to spin a thread by default
@@ -86,19 +82,17 @@ int main(int argc, char** argv){
 
   //double timeout = 40.0;  // secs
   double timeout = 3600.0;  // secs
-
-  double x = -10.0;         // Set goal
-  double y = 0.0;
-  double w = -1.0;
-
   double last_time = ros::Time::now().toSec();
 
-  goal.target_pose.pose.position.x = x; // Send goal
-  goal.target_pose.pose.position.y = y;
-  goal.target_pose.pose.orientation.w = w;
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
-  ros::Duration(0.5).sleep();   // Sleep for half a second
+  double goal_x;         // Set goal
+  double goal_y;
+  double goal_w;
+
+  int value;        // Gridmap vars
+  double x;
+  double y;
+
+  bool goal_sent = false;
 
   // Loop rate
   ros::Rate loop_rate(1);	// 1Hz
@@ -106,44 +100,61 @@ int main(int argc, char** argv){
   while (ros::ok()) {
 
     ROS_INFO("---------------------------------------------------------------");
-    ROS_INFO("Origin: [%f,%f,%f]", gridmap.pos.x, gridmap.pos.y, gridmap.dir.w);
-    ROS_INFO("Height: %d\tWidth: %d\tSize: %d", gridmap.height, gridmap.width, /*(int)gridmap.data.size()*/ gridmap.height*gridmap.width);
-
-    /*for(int i = 0; i < gridmap.data.size(); i++)
-    {
-        ROS_INFO("\t%d",(int) gridmap.data[i]);
-    }*/
-
-    for(int i = 0; i < gridmap.width; i++)
-      for(int j = 0; j < gridmap.height; j++)
-        ROS_INFO("[%d][%d]:\t%d", i, j, gridmap.grid[i][j]);
-
-    //ROS_INFO("time: %f,\tdist: %f,\trot: %f", odom.stamp-last_time, pow( pow(odom.x - x, 2) + pow(odom.y - y, 2), 0.5), fabs(odom.w - w));
-    //ROS_INFO("result: %d", system(ac.getState().getText().c_str()));
-
-    ROS_INFO("int: %d", system(ac.getState().getText().c_str()));
+    ROS_INFO("Time: %f\tResolution: %f\tOrigin: [%f,%f,%f]", gridmap.stamp, gridmap.resolution, gridmap.pos.x, gridmap.pos.y, gridmap.dir.w);
+    ROS_INFO("Height: %d\tWidth: %d\tSize: %d", gridmap.height, gridmap.width, (int)gridmap.data.size());
     
-    if( pow( pow(odom.x - x, 2) + pow(odom.y - y, 2), 0.5) < 0.1  // Distance from goal
-      && fabs(odom.w - w) < 0.1 )                                 // Difference in orientation
-    {
-      ac.cancelAllGoals();
-      ROS_INFO("\n\tGoal reached");
-      ros::Duration(1.0).sleep();   // Sleep for a second
-      break;
+    if (!goal_sent){
+      for(int j = 0; j < gridmap.height; j++){
+        y = j * gridmap.resolution + gridmap.pos.y;   // y coordinate in meters
+        for(int i = 0; i < gridmap.width; i++){
+          x = i * gridmap.resolution + gridmap.pos.x; // x coordinate in meters
+          value = gridmap.data[j*gridmap.width + i];
+          if (value == 100){
+            goal_x = x;
+            goal_y = y;
+            goal_w = 1.0;
+            goal.target_pose.pose.position.x = goal_x; // Send goal
+            goal.target_pose.pose.position.y = goal_y;
+            goal.target_pose.pose.orientation.w = goal_w;
+
+            last_time = ros::Time::now().toSec();
+
+            ROS_INFO("Sending goal");
+            ac.sendGoal(goal);
+            goal_sent = true;
+            break;
+          }
+          //printf("%d ", index);
+        }
+        //printf("\n");
+      }
     }
-    else if ( odom.stamp - last_time > timeout || ac.getState() == actionlib::SimpleClientGoalState::ABORTED)  // Timeout reached
-    {
-      ac.cancelAllGoals();
-      ROS_INFO("\n\tCould not reach goal");
-      ros::Duration(1.0).sleep();   // Sleep for a second
-      break;
+
+    //ROS_INFO("time: %f,\tdist: %f,\trot: %f", odom.stamp-last_time, pow( pow(odom.x - goal_x, 2) + pow(odom.y - goal_y, 2), 0.5), fabs(odom.w - goal_w));
+    
+    if (goal_sent) {
+      if( pow( pow(odom.x - goal_x, 2) + pow(odom.y - goal_y, 2), 0.5) < 0.1  // Distance from goal
+        && fabs(odom.w - goal_w) < 0.1 )                                 // Difference in orientation
+      {
+        ac.cancelAllGoals();
+        ROS_INFO("\n\tGoal reached");
+        goal_sent = false;
+        ros::Duration(1.0).sleep();   // Sleep for a second
+        break;
+      }
+      else if ( odom.stamp - last_time > timeout || ac.getState() == actionlib::SimpleClientGoalState::ABORTED)  // Timeout reached
+      {
+        ac.cancelAllGoals();
+        ROS_INFO("\n\tCould not reach goal");
+        goal_sent = false;
+        ros::Duration(1.0).sleep();   // Sleep for a second
+        break;
+      }
     }
 
     ros::spinOnce();
     loop_rate.sleep();
   }
-
-  last_time = odom.stamp;
 
   return 0;
 }
